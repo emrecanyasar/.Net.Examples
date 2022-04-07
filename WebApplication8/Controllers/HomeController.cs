f59d0599a4660fe9d56cb2c8398ca673f6f6c411
Emrecan8 <emrecanyasar8@outlook.com>
2022-04-07 13:36:47 +0300

    ioc container

---
+++ WebApplication8/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication8.Models;
+using WebApplication8.Services.SmsService;
+using System.Diagnostics;
+
+namespace WebApplication8.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ISmsService _smsService;
+
+        public HomeController(ISmsService smsService)
+        {
+            _smsService = smsService;
+        }
+
+
+        public IActionResult Index()
+        {
+            var result = _smsService.Send(new SmsModel()
+            {
+                TelefonNo = "12345",
+                Mesaj = "home/index çalıştı"
+            });
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
