b53207c4fec89529fe2f5e3fc312b6f293cf5895
Emrecan8 <emrecanyasar8@outlook.com>
2022-04-13 14:40:17 +0300

    profile

--- Identity101/Controllers/AccountController.cs
+++ Identity101/Controllers/AccountController.cs
@@ -259,4 +259,45 @@ public class AccountController : Controller
             return View();
         }
     }
+    [Authorize]
+    [HttpGet]
+    public async Task<IActionResult> Profile()
+
+    {
+        var user = await _userManager.FindByNameAsync(HttpContext.User.Identity!.Name);
+
+        var model = new UserProfileViewModel()
+        {
+            Email = user.Email,
+            Name = user.Name,
+            Surname = user.Surname
+        };
+        return View(model);
+    }
+    [Authorize]
+    [HttpPost]
+    public async Task<IActionResult> Profile(UserProfileViewModel model)
+    {
+        if (!ModelState.IsValid)
+            return View(model);
+        var user = await _userManager.FindByNameAsync(HttpContext.User.Identity!.Name);
+        user.Name = model.Name;
+        user.Surname = model.Surname;
+        user.Email = model.Email;
+
+        var result = await _userManager.UpdateAsync(user);
+        if (result.Succeeded)
+        {
+            ViewBag.Message = "Güncelleme başarılı";
+        }
+        else
+        {
+            var message = string.Join("<br>", result.Errors.Select(x => x.Description));
+            ViewBag.Message = message;
+        }
+        return View(model);
+
+
+
+    }
 }
