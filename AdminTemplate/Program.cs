9f21e0bdc6708be9589ee66f4a89feb39af54947
Emrecan8 <emrecanyasar8@outlook.com>
2022-04-19 10:21:12 +0300

    tlh

--- AdminTemplate/Program.cs
+++ AdminTemplate/Program.cs
@@ -25,6 +25,6 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-app.MapRazorPages();
+//app.MapRazorPages();
 
 app.Run();
